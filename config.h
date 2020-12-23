04b67bafa3ca5ba5dc12e07f308166e15f4476a0
jiho <ohijiho@gmail.com>
2020-12-23 19:20:25 +0900

    final commit

--- config.h
+++ config.h
@@ -4,13 +4,15 @@
 #define __VITIS_HLS__
 #endif
 
-#define BATCH_SIZE 8
 #define VALUE_SIZE 32
 #define VALUE_FLOAT 0
 #define VALUE_INT_PART 8
 #define INT_SIZE 32
 #define INT_PRIMITIVE 1
-#define PACK_W 5
+
+#define BATCH_SIZE 1
+#define PACK_W 1
+
 #define LAYER6_TANH_CPU 1
 #define REUSE_LAYER_FUNCTIONS 1
 #define CONV_DATAFLOW 1
